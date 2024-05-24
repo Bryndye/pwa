8db6a98d4b2b746abc10ac4f55a296f66c6bbaf5
Bryndye <hugocluzel17@hotmail.fr>
2024-05-24 09:42:01 +0200

    [DEV] phone call

--- src/components/phone.js
+++ src/components/phone.js
@@ -1,18 +1,12 @@
 
 function Phone(){
-    const isMobile = () => {
-        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    }
-
     const handlePhoneCall = (phone_number) => {
         window.location.href = `tel:${phone_number}`;
     }
 
     return (
         <div>
-            {
-                isMobile() ?? <button onClick={() => handlePhoneCall('0771225772')}>Appeler</button>
-            }
+            <button onClick={() => handlePhoneCall('0771225772')}>Appeler</button>
         </div>
     )
 }
